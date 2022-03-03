7d64cd6fe1bd21489321f75f650c5c3707c70a4a
sherany <sheranymcpoca@gmail.com>
2022-03-03 11:00:01 -0300

    none

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -13,7 +13,55 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
+import java.awt.EventQueue;
+import java.awt.FileDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Insets;
+import javax.swing.JMenu;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.print.PrinterException;
+import java.awt.event.InputEvent;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
+import java.awt.event.KeyAdapter;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.event.MenuListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.UndoManager;
+import javax.swing.event.MenuEvent;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.CaretListener;
+import javax.swing.event.CaretEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.Window.Type;
 import java.awt.Font;
 import javax.swing.JTextField;
@@ -40,12 +88,16 @@ class MyFrame extends JFrame {
     private JButton editButton = new JButton("Edit");
     private JButton findFileButton = new JButton("Find file");
 
-
-    StoryButtonListener buttonListener = new StoryButtonListener();
+        StoryButtonListener buttonListener = new StoryButtonListener();
 
 //    ReplaceDialogButtonListener replaceListener = new ReplaceDialogButtonListener();
 
     private JTextArea textArea = new JTextArea();
+
+    private JPanel panel = new JPanel();
+//    private JScrollPane scrollableTextArea = new JScrollPane();
+
+
 
     private JTextField fileNameTextField = new JTextField();
     private JTextField txtobject = new JTextField();
@@ -64,7 +116,7 @@ class MyFrame extends JFrame {
 
     public MyFrame() {
         setTitle("Storymaker");
-        setSize(500, 400);
+        setSize(700, 400);
         setLocation(new Point(300, 200));
         setLayout(null);
         setResizable(true);
@@ -89,10 +141,9 @@ class MyFrame extends JFrame {
 
         lblinstr.setBounds(210, 35, 200, 20);
 
-        textArea.setBounds(0, 90, 800, 240);
+        textArea.setBounds(0, 90, 700, 240);
 
-
-        saveButton.setBounds(20, 340, 80, 20);
+                saveButton.setBounds(20, 340, 80, 20);
         loadButton.setBounds(120, 340, 80, 20);
         clearButton.setBounds(220, 340, 80, 20);
         editButton.setBounds(320, 340, 80, 20);
@@ -110,6 +161,7 @@ class MyFrame extends JFrame {
 //        add(getTxtPlaceInput);
 
         add(textArea);
+//        add(scrollableTextArea);
 
         add(saveButton);
 //        add(loadButton);
@@ -289,14 +341,14 @@ class MyFrame extends JFrame {
                         //all j components
                         JPanel contentPanel = new JPanel();
 
-                        JTextField findObject = new JTextField();
-                        JTextField replaceObject = new JTextField();
+                        JTextField findA = new JTextField();
+                        JTextField replaceA = new JTextField();
 
-                        JTextField findPlace = new JTextField();
-                        JTextField replacePlace = new JTextField();
+                        JTextField findB = new JTextField();
+                        JTextField replaceB = new JTextField();
 
                         JButton findNextButton = new JButton("Find Next");
-                        JCheckBox wrapAroundButton = new JCheckBox("Wrap around");
+                        JLabel instrLabel = new JLabel("Find and replace country first and then the object");
                         JButton replaceAllButton = new JButton("Replace");
 //                JButton replaceButton = new JButton("Replace");
                         JButton cancelButton = new JButton("Cancel");
@@ -305,10 +357,10 @@ class MyFrame extends JFrame {
                         a.getContentPane().add(contentPanel);
                         contentPanel.setLayout(null);
 
-                        findObject.addKeyListener(new KeyAdapter() {
+                        findA.addKeyListener(new KeyAdapter() {
                             @Override
                             public void keyTyped(KeyEvent e) {
-                                if (findObject.getText() != null) {
+                                if (findA.getText() != null) {
                                     findNextButton.setEnabled(true);
 //                            replaceButton.setEnabled(true);
                                     replaceAllButton.setEnabled(true);
@@ -320,10 +372,10 @@ class MyFrame extends JFrame {
                             }
                         });
 
-                        findPlace.addKeyListener(new KeyAdapter() {
+                        findB.addKeyListener(new KeyAdapter() {
                             @Override
                             public void keyTyped(KeyEvent e) {
-                                if (findPlace.getText() != null) {
+                                if (findB.getText() != null) {
                                     findNextButton.setEnabled(true);
 //                            replaceButton.setEnabled(true);
                                     replaceAllButton.setEnabled(true);
@@ -336,42 +388,42 @@ class MyFrame extends JFrame {
                         });
 
                         //find Object field
-                        findObject.setBounds(100, 21, 220, 30);
-                        contentPanel.add(findObject);
-                        findObject.setColumns(10);
+                        findA.setBounds(100, 21, 220, 30);
+                        contentPanel.add(findA);
+                        findA.setColumns(10);
 
-                        JLabel findObjectLabel = new JLabel("Find Object:");
-                        findObjectLabel.setLabelFor(findObject);
+                        JLabel findObjectLabel = new JLabel("Find:");
+                        findObjectLabel.setLabelFor(findA);
                         findObjectLabel.setBounds(10, 20, 100, 30);
                         contentPanel.add(findObjectLabel);
 
                         //replace Object field
-                        replaceObject.setColumns(10);
-                        replaceObject.setBounds(100, 55, 220, 30);
-                        contentPanel.add(replaceObject);
+                        replaceA.setColumns(10);
+                        replaceA.setBounds(100, 55, 220, 30);
+                        contentPanel.add(replaceA);
 
                         JLabel replaceObjectLabel = new JLabel("Replace with:");
-                        replaceObjectLabel.setLabelFor(replaceObject);
+                        replaceObjectLabel.setLabelFor(replaceA);
                         replaceObjectLabel.setBounds(10, 55, 100, 30);
                         contentPanel.add(replaceObjectLabel);
 
                         //find Place field
-                        findPlace.setBounds(100, 100, 220, 30);
-                        contentPanel.add(findPlace);
-                        findPlace.setColumns(10);
+                        findB.setBounds(100, 100, 220, 30);
+                        contentPanel.add(findB);
+                        findB.setColumns(10);
 
-                        JLabel findPlaceLabel = new JLabel("Find Place:");
-                        findPlaceLabel.setLabelFor(findObject);
+                        JLabel findPlaceLabel = new JLabel("Find:");
+                        findPlaceLabel.setLabelFor(findB);
                         findPlaceLabel.setBounds(10, 100, 100, 30);
                         contentPanel.add(findPlaceLabel);
 
                         //replace Place field
-                        replacePlace.setColumns(10);
-                        replacePlace.setBounds(100, 134, 220, 30);
-                        contentPanel.add(replacePlace);
+                        replaceB.setColumns(10);
+                        replaceB.setBounds(100, 134, 220, 30);
+                        contentPanel.add(replaceB);
 
                         JLabel replacePlaceLabel = new JLabel("Replace with:");
-                        replacePlaceLabel.setLabelFor(replaceObject);
+                        replacePlaceLabel.setLabelFor(replaceB);
                         replacePlaceLabel.setBounds(10, 134, 100, 30);
                         contentPanel.add(replacePlaceLabel);
 
@@ -386,8 +438,8 @@ class MyFrame extends JFrame {
                         contentPanel.add(cancelButton);
 
                         //wordwrap button
-                        wrapAroundButton.setBounds(6, 178, 117, 23);
-                        contentPanel.add(wrapAroundButton);
+                        instrLabel.setBounds(6, 178, 300, 23);
+                        contentPanel.add(instrLabel);
 
 //                //replace button
 //                replaceButton.addActionListener(new ActionListener() {
@@ -461,11 +513,11 @@ class MyFrame extends JFrame {
                         //replace button
                         replaceAllButton.addActionListener(new ActionListener() {
                             public void actionPerformed(ActionEvent e) {
-                                String patternA = findObject.getText();
-                                String replaceTextA = replaceObject.getText();
+                                String patternA = findA.getText();
+                                String replaceTextA = replaceA.getText();
 
-                                String patternB = findPlace.getText();
-                                String replaceTextB = replacePlace.getText();
+                                String patternB = findB.getText();
+                                String replaceTextB = replaceB.getText();
 
 
                                 Pattern pA = Pattern.compile(patternA);
@@ -477,29 +529,24 @@ class MyFrame extends JFrame {
 
                                 //start from starting and replace all matching with replace Text
                                 int currentposition = 0;
-                                while (mA.find(currentposition)) {
+                                while (mA.find(currentposition) && mB.find(currentposition)) {
                                     textArea.select(mA.start(), mA.end());
                                     textArea.replaceSelection(replaceTextA);
+
+                                    textArea.select(mB.start(), mB.end());
+                                    textArea.replaceSelection(replaceTextB);
+
                                     //get position and matcher of new content
                                     currentposition = textArea.getCaretPosition();
                                     mA = pA.matcher(textArea.getText());
-                                }
-                                //set caret position at start
-                                textArea.setCaretPosition(0);
 
-
-                                if (mB.find(currentposition)) {
-                                    textArea.select(mB.start(), mB.end());
-                                    textArea.replaceSelection(replaceTextA);
-
-                                    //get position and matcher of new content
                                     currentposition = textArea.getCaretPosition();
                                     mB = pB.matcher(textArea.getText());
+
+
                                 }
                                 //set caret position at start
                                 textArea.setCaretPosition(0);
-
-
 
                             }
                         });
